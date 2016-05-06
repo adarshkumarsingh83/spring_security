96518d544317a165a26394c5fa0babd089b46f71
adarkuma <adarkuma@cisco.com>
2016-05-06 10:09:56 +0530

    adding

--- SpringSecurityHibernateCustomUserDetailsApplication/src/main/java/com/adarsh/spring/repository/construct/Repository.java
+++
@@ -1,9 +0,0 @@
-package com.adarsh.spring.repository.construct;
-
-/**
- * @author Adarsh
- * @author $LastChangedBy: adarsh $
- * @version $Revision: 1595 $, $Date:: 5/4/12 6:12 PM#$
- */
-public interface Repository  {
-}
--- SpringSecurityJersyRestTokenAuthentication/src/main/webapp/index.jsp
+++
@@ -1,4 +0,0 @@
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<div style="color: darkgreen">
-    <h1>WELCOME TO THE APPLICATION</h1>
-</div>
