2f5db142da4c95bf9449be6ffb04dee13a662623
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2016-05-26 21:09:06 +0530

    SpringSecurityDatabaseQueryAnnotatonExample

---
+++ SpringSecurityDatabaseQueryAnnotatonExample/src/main/java/com/espark/adarsh/configuration/persistence/PersistenceConfiguration.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2015 Espark And ©Adarsh Development Services @copyright All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Espark nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.espark.adarsh.configuration.persistence;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+
+/**
+ * @author Adarsh Kumar
+ * @author $LastChangedBy: Adarsh Kumar$
+ * @version $Revision: 0001 $, $Date:: 1/1/10 0:00 AM#$
+ * @Espark @copyright all right reserve
+ */
+@Configuration
+@PropertySource("classpath:properties/application.properties")
+public class PersistenceConfiguration {
+
+    @Value("${dataSource.driverClass}")
+    private String driverClassName;
+    @Value("${dataSource.url}")
+    private String url;
+    @Value("${dataSource.hostname}")
+    private String hostName;
+    @Value("${dataSource.portNumber}")
+    private String portNumber;
+    @Value("${dataSource.database}")
+    private String databaseName;
+    @Value("${dataSource.extraParam}")
+    private String extraPram;
+    @Value("${dataSource.username}")
+    private String userName;
+    @Value("${dataSource.password}")
+    private String userPwd;
+
+
+    @Bean
+    public DataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url + hostName + portNumber + databaseName + extraPram);
+        dataSource.setUsername(userName);
+        dataSource.setPassword(userPwd);
+        DatabasePopulatorUtils.execute(createDatabasePopulator(), dataSource);
+        return dataSource;
+    }
+
+
+    private DatabasePopulator createDatabasePopulator() {
+        ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
+        databasePopulator.setContinueOnError(true);
+        databasePopulator.addScript(new ClassPathResource("espark.sql"));
+        return databasePopulator;
+    }
+
+}
---
+++ SpringSecurityDatabaseQueryAnnotatonExample/src/main/webapp/WEB-INF/pages/accessDenied.jsp
@@ -0,0 +1,20 @@
+<%--
+ * @author Adarsh Kumar
+ * @author $LastChangedBy: Adarsh Kumar$
+ * @version $Revision: 0001 $, $Date:: 1/1/10 0:00 AM#$
+ * @Espark @copyright all right reserve
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <link rel="stylesheet" href="../static/css/bootstrap.min.css"/>
+    <script type="text/javascript" src="../static/js/jquery.js"></script>
+    <script type="text/javascript" src="../static/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div style="text-align: center">
+    <h1>INSUFFICIENT PRIVILEGE</h1>
+</div>
+</body>
+</html>
