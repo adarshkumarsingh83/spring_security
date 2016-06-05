0876d08f8dae268b76ceff5f5647af375b642a73
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2016-06-05 17:50:04 +0530

    SpringSecurityRestEasyTokenAuthentication

---
+++ SpringSecurityRestEasyTokenAuthentication/src/main/java/com/adarsh/spring/resteasy/rest/RestAuthenticationController.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright (c) 2015 Espark And ©Adarsh Development Services @copyright All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Espark nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.adarsh.spring.resteasy.rest;
+
+
+import com.adarsh.spring.resteasy.bean.ApiServer;
+import com.adarsh.spring.resteasy.persistance.entity.User;
+import com.adarsh.spring.resteasy.security.SecurityContextProvider;
+import com.adarsh.spring.resteasy.security.TokenProvider;
+import com.adarsh.spring.resteasy.service.UserService;
+import com.adarsh.spring.resteasy.util.SpringRestApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Map;
+
+/**
+ * @author Adarsh Kumar
+ * @author $LastChangedBy: Adarsh Kumar$
+ * @version $Revision: 0001 $, $Date:: 1/1/10 0:00 AM#$
+ * @Espark @copyright all right reserve
+ */
+@Path("/authenticate")
+public class RestAuthenticationController {
+
+    private static final Logger logger = LoggerFactory.getLogger(RestAuthenticationController.class);
+
+    private UserService userService = (UserService) SpringRestApplicationContext.getBean(UserService.class);
+
+    private UserDetailsService userDetailsService=(UserDetailsService)SpringRestApplicationContext.getBean(UserDetailsService.class);
+
+    private TokenProvider tokenProvider=(TokenProvider)SpringRestApplicationContext.getBean(TokenProvider.class);
+
+    private AuthenticationManager authenticationManager=(AuthenticationManager)SpringRestApplicationContext.getBean("authenticationManager");
+
+    private SecurityContextProvider securityContextProvider = new SecurityContextProvider();
+
+    private final String STATUS = "failure";
+
+    private final String MESSAGE = "Invalid Username or Password";
+
+    private final String USERNAME = "username";
+
+    private final String PASSWORD = "password";
+
+    @POST
+    @Path("/login")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response authenticate(Map<String, String> map,@Context HttpServletResponse httpServletResponse) {
+        final ApiServer apiServer = new ApiServer();
+        logger.debug("Inside-AuthenticationController.java");
+
+        String token = null;
+        final String username = map.get(USERNAME);
+        final String password = map.get(PASSWORD);
+        User user = null;
+        try {
+            user = userService.getUser(username);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        if (user == null) {
+            final UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username, password);
+            final boolean result = this.authenticationManager.authenticate(authentication).isAuthenticated();
+            if (result) {
+                user = new User();
+                user.setLogin(username);
+                user.setPassword(password);
+            }
+        }
+
+
+        if (user != null && user.getPassword().equals(password)) {
+            token = tokenProvider.getToken(user);
+            httpServletResponse.setHeader("Authentication-token", token);
+            apiServer.setToken(token);
+            apiServer.setUserName(username);
+            apiServer.setUserPassword(password);
+            apiServer.setMessage("LOGIN SUCCESSFUL ");
+            apiServer.setHttpStatus(HttpServletResponse.SC_OK);
+        } else {
+            apiServer.setMessage("LOGIN UNSUCCESSFUL ");
+            apiServer.setHttpStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+        return Response.status(200).entity(apiServer).build();
+    }
+
+}
