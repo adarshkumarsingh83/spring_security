7af984cf3a448b3fb8118ba68d35b6db0d3a1c8c
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2016-06-23 19:13:43 +0530

    SpringSecurityRestMvcTokenAuthenticationAnnotationExample

---
+++ SpringSecurityRestMvcTokenAuthenticationAnnotationExample/src/main/java/com/espark/adarsh/rest/configuration/security/RestWebSecurityConfiguration.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2015 Espark And ©Adarsh Development Services @copyright All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Espark nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.espark.adarsh.rest.configuration.security;
+
+
+import com.espark.adarsh.rest.security.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+/**
+ * @author Adarsh Kumar
+ * @author $LastChangedBy: Adarsh Kumar$
+ * @version $Revision: 0001 $, $Date:: 1/1/10 0:00 AM#$
+ * @Espark @copyright all right reserve
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@ComponentScan({"com.espark.adarsh.rest.security.*"})
+public class RestWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    public void configure(WebSecurity webSecurity) throws Exception {
+        webSecurity.ignoring().antMatchers("/static/js/*.js")
+                .antMatchers("/static/css/*.css")
+                .antMatchers("/static/images/*.ico")
+                .antMatchers("/index.jsp")
+                .antMatchers("/rest/authenticate/login");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/rest/*")
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/rest/authenticate/login").permitAll()
+                .antMatchers("/rest/welcome").hasAnyAuthority(new String[]{"ROLE_SUPERADMIN", "ROLE_ADMIN", "ROLE_USER"})
+                .antMatchers("/rest/**").authenticated()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic().disable()
+                .formLogin().disable()
+                .rememberMe().disable()
+                .requestCache().disable()
+                .x509().disable()
+                .logout()
+                .logoutUrl("/rest/logout")
+                .logoutSuccessHandler(this.getRestLogoutSuccessHandler())
+                .and()
+                // add custom authentication filter
+                .addFilterBefore(this.getAuthenticationTokenProcessingFilter(), BasicAuthenticationFilter.class)
+                        // register custom authentication exception handler
+                .exceptionHandling().authenticationEntryPoint(this.getEntryPointBean())
+                .accessDeniedHandler(this.getAccessDeniedHandler());
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(getRestApiAuthProvider());
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean(name = "authenticationProvider")
+    public AuthenticationProvider getRestApiAuthProvider() {
+        return new RestApiAuthProvider();
+    }
+
+    @Bean
+    public AuthenticationTokenProcessingFilter getAuthenticationTokenProcessingFilter() throws Exception {
+        return new AuthenticationTokenProcessingFilter(this.authenticationManager(), this.tokenService());
+    }
+
+    @Bean(name = "entryPoint")
+    public ApiServerAuthenticationEntryPoint getEntryPointBean() {
+        return new ApiServerAuthenticationEntryPoint();
+    }
+
+    @Bean(name = "accessDeniedHandler")
+    public RestAccessDeniedHandler getAccessDeniedHandler() {
+        return new RestAccessDeniedHandler();
+    }
+
+    @Bean(name = "restLogoutSuccessHandler")
+    public RestLogoutSuccessHandler getRestLogoutSuccessHandler() {
+        return new RestLogoutSuccessHandler();
+    }
+
+    @Bean(name = "md5PasswordEncoder")
+    public Md5PasswordEncoder getEncoder() {
+        return new Md5PasswordEncoder();
+    }
+
+    @Bean
+    public TokenService tokenService() {
+        return new TokenService();
+    }
+
+}
---
+++ SpringSecurityRestMvcTokenAuthenticationAnnotationExample/src/main/webapp/index.jsp
@@ -0,0 +1,29 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%--
+* @author Adarsh Kumar
+* @author $LastChangedBy: Adarsh Kumar$
+* @version $Revision: 0001 $, $Date:: 1/1/10 0:00 AM#$
+* @Espark @copyright all right reserve
+--%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title>Login</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <link rel="stylesheet" href="static/css/bootstrap.css"/>
+    <script type="text/javascript" src="static/js/jquery.js"></script>
+    <script type="text/javascript" src="static/js/bootstrap.js"></script>
+</head>
+<body style="background-color: #31b0d5">
+<div class="espark" style="text-align: center">
+    <div class="container">
+        <div class="jumbotron" style="text-align: center">
+            <div class="alert alert-info">
+                <h3>ESPARK SPRING WEB MVC ANNOTATION REST TOKEN AUTHENTICATION </h3>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
