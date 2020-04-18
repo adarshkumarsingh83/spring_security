f4c0939620bb537ddb5284d3457d2be59e48bb28
Kumar <adarshkumarsingh83@gmail.com>
2020-04-17 20:16:18 -0500

    updating

--- SPRING-BOOT-OAUTH-MS/springboot-oauth-server-db/src/main/java/com/espark/adarsh/config/AuthorizationServerConfig.java
+++ SPRING-BOOT-OAUTH-MS/springboot-oauth-server-db/src/main/java/com/espark/adarsh/config/AuthorizationServerConfig.java
@@ -2,6 +2,7 @@ package com.espark.adarsh.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -9,6 +10,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 import javax.sql.DataSource;
 
@@ -27,7 +29,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Value("${espark.oauth2.authorization.check-token-access}")
     private String checkTokenAccess;
 
-
     @Override
     public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
         oauthServer
@@ -43,5 +44,11 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.authenticationManager(authenticationManager);
+        endpoints.tokenStore(jdbcTokenStore());
+    }
+
+    @Bean
+    public JdbcTokenStore jdbcTokenStore() {
+        return new JdbcTokenStore(dataSource);
     }
 }
